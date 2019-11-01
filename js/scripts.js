2238cd169963964d0391254c435ae2ab141fdd42
Student <example@example.com>
2019-11-01 16:21:59 -0700

    save everything else

--- js/scripts.js
+++ js/scripts.js
@@ -4,53 +4,58 @@ $(document).ready(function() {
 
 var q1 = $("#q1").val();
 var existence;
+var existences = [];
+
+$("#hi").click(function(event) {
+  $("body").removeClass();
+  $("body").addClass("asylum");
+});
 
 
-// $("#hi").click(function(event) {
-//   $("body").removeClass();
-//   $("body").addClass("asylum");
-// });
-//
-//
-//
-//
-// for (i = 0; i < q1; i++) {
-//
-// if (q1 % 3 === 0 && q1 % 5 === 0) {
-//   existence = [q1 + ": BEEP! BOOP! I SEE YOU HAHAHAHAHAHAHAHAHAHA!!! I CAN'T HANDLE THESE NUMBERS!!!!!!!!!!!!!!!"];
-// } else if (q1 % 3 === 0) {
-//   existence = [q1 + ": Beep!"];
-//
-// } else if (q1 % 5 === 0) {
-//   existence = [q1 + ": Boop!"];
-//
-// } else {
-//   existence = ("You have to enter the asylum in order to actually win against the creatures within...");
-//   }
-// }
-//
-// if (existence === "You have to enter the asylum in order to actually win against the creatures within..." ) {
-//   $("body").removeClass();
-//   $(".container").hide();
-//   $("#bot1").hide();
-//   $("body").addClass("outside");
-//   $("#bot2").show();
-//   $(".invisiclass").show();
-// } else if (existence === ": BEEP! BOOP! I SEE YOU HAHAHAHAHAHAHAHAHAHA!!! I CAN'T HANDLE THESE NUMBERS!!!!!!!!!!!!!!!") {
-//   // $("body").removeClass();
-//   // $(".container").hide();
-//   // $("#bot1").hide();
-//   $("body").addClass("explosion");
-//   // $(existence).click(function() {
-//
-//   $("#bot2").show();
-// } else {
-//
-// }
+
+if (q1 % 3 === 0 && q1 % 5 === 0) {
+  existence = [q1 + ": BEEP! BOOP! I SEE YOU HAHAHAHAHAHAHAHAHAHA!!! I CAN'T HANDLE THESE NUMBERS!!!!!!!!!!!!!!! "];
+} else if (q1 % 3 === 0) {
+  existence = [q1 + ": Beep! "];
+
+} else if (q1 % 5 === 0) {
+  existence = [q1 + ": Boop! "];
+
+} else if (q1 % 2 === 1) {
+  prompt("BEEP");
+  existence = [q1 + ": BEEEEEEP!! BEEEEEEP!! BEEEEEEP!! BEEEEEEP!! BEEEEEEP!! "];
 
 
-console.log("data is", q1);
+} else if (q1 === 0) {
+  existence = [q1 + ": Boop! "];
 
-    $("#emptyDiv").text(existence);
+} else {
+  existence = ("You have to enter the asylum in order to actually win against the creatures within...");
+}
+
+
+
+
+if (existence === "You have to enter the asylum in order to actually win against the creatures within..." ) {
+  $("body").removeClass();
+  $(".container").hide();
+  $("#bot1").hide();
+  $("body").addClass("outside");
+  $("#bot2").show();
+  $(".invisiclass").show();
+} else if (q1 % 3 === 0 && q1 % 5 === 0){
+  console.log("data is", q1);
+  $("body").removeClass();
+  $(".container").hide();
+  $("#bot1").hide();
+  $("body").addClass("explosion");
+  $("#bot2").show();
+} else {
+}
+
+
+
+    $("#emptyDiv").append(existence);
+  }
   });
 });
